1f0c1ae74f203ef4521c29714820ecd71725eb4b
LapMarcos <marcoslaporte2015@gmail.com>
2022-01-03 04:27:20 -0300

    🛠 Mantenimiento.

--- Botones.js
+++ Botones.js
@@ -46,8 +46,8 @@ let btnBuscar = document.querySelector("#buscar");
 		}
 		for(let i = 1; i < pelis.length; i++){
 			if(busqueda == pelis[i]){
-				alert(`"${busqueda}" se encuentra en el número ${i}.`);
-				console.log(`"${busqueda}" se encuentra en el número ${i}.`);
+				alert(`"${busqueda}" es el número ${i}.\n<-Línea ${linea[i]}->`);
+				console.log(`"${busqueda}" es el número ${i}. <-Línea ${linea[i]}->`);
 				return;
 			}else if(i == 1000){
 				alert(`"${busqueda}" no existe. El título que escriba debe coincidir con alguno de la lista.`);
@@ -56,21 +56,14 @@ let btnBuscar = document.querySelector("#buscar");
 		}
 	}
 
-let min = 1;
-let max = 1001;
+const min = 1;
+const max = 1001;
 let random;
-let SEEN = [ , 1, 7, 8, 12, 19, 20, 21, 22, 24, 25, 28, 31, 35, 38, 54, 65, 69, 76, 80, 82, 92, 93, 103, 104, 111, 112, 129, 135, 151, 152, 157, 165, 167, 188, 190, 192, 208, 233, 241, 248, 273, 281, 282, 283, 284, 285, 289, 297, 303, 305, 310, 311, 319, 320, 323, 339, 358, 364, 383, 403,
-404, 408, 409, 416, 435, 456, 462, 473, 487, 492, 508, 514, 517, 535, 545, 553, 555, 583, 587, 591, 601, 604, 607, 619, 630, 639, 671, 685, 688, 690, 695, 716, 717, 733, 735, 752, 756, 767, 772, 776, 780, 785, 787, 869, 876, 877, 882, 886, 897, 903, 919, 932, 937, 939, 945, 959, 994];
-let BEST = [ , 5, 27, 73, 81, 185, 226, 246, 265, 414, 437, 465, 467, 533, 598, 745, 842, 954];
-let BAD = [ , 106, 529, 530, 758];
-let nameSEEN = [];
-let nameBEST = [];
-let nameBAD = [];
 	
 	// Marca películas vistas
 	btnSeen.onclick = function(){
 		let contSeen = 0;
-		for(let i = 1; i < pelis.length; i++){
+		for(let i = min; i < pelis.length; i++){
 			if(pelis[SEEN[i]] != undefined){
 			console.log(pelis[SEEN[i]]);
 			contSeen++;
@@ -87,7 +80,7 @@ let nameBAD = [];
 	// Marca las mejores películas
 	btnBest.onclick = function(){
 		let contBest = 0;
-		for(var i = 1; i < pelis.length; i++){
+		for(let i = min; i < pelis.length; i++){
 			if(pelis[BEST[i]] != undefined){
 			console.log(pelis[BEST[i]]);
 			contBest++;
@@ -104,7 +97,7 @@ let nameBAD = [];
 	// Marca las peores películas
 	btnBad.onclick = function(){
 		let contBad = 0;
-		for(let i = 1; i < pelis.length; i++){
+		for(let i = min; i < pelis.length; i++){
 			if(pelis[BAD[i]] != undefined){
 			console.log(pelis[BAD[i]]);
 			contBad++;
@@ -121,14 +114,14 @@ let nameBAD = [];
 	btnRandom.onclick = function(){
 		random = Math.floor(Math.random() * max + min);
 			if(SEEN.includes(random)){
-				alert(`Número ${random}? Esta película ya la viste! Intentá de nuevo.`);
+				alert(`"${pelis[random]}"? Esta película ya la viste! Intentá de nuevo.`);
 			}else if(BEST.includes(random)){
-				alert(`Número ${random}? Una joyita! Ya la viste, pero da para verla de nuevo! Intentá de nuevo.`);
+				alert(`"${pelis[random]}"? Una joyita! Ya la viste, pero da para verla de nuevo! Intentá de nuevo.`);
 			}else if(BAD.includes(random)){
-				alert(`Número ${random}? Que película de mierda! Ya la viste. Intentá de nuevo.`);
+				alert(`"${pelis[random]}"? Que película de mierda! Ya la viste. Intentá de nuevo.`);
 			}else{
-				alert(`La película elegida es la número ${random}!`);
+				alert(`La película elegida es "${pelis[random]}"!`);
 			}
-		alert(`La película con ese número (${random}) es: ${pelis[random]}.`);
-		console.log(`${random}, ${pelis[random]}`);
+		// alert(`La película con ese número (${random}) es: ${pelis[random]}.\n<-Línea ${linea[random]}->`);
+		console.log(`${random}, ${pelis[random]}. <-${linea[random]}->`);
 	}
